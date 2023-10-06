91fdfe38502fa4347d56f4522a0fab406248c5c7
Ilya <senognoevilya@gmail.com>
2023-10-06 22:26:43 +0600

    change config webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,20 +1,24 @@
 import path from 'path'
 import webpack from 'webpack'
 import HTMLWebpackPlugin from 'html-webpack-plugin'
+import Dotenv from 'dotenv-webpack'
 
 const config: webpack.Configuration = {
-  mode: 'development',
   entry: path.resolve(__dirname, 'src', 'index.ts'),
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'build'),
     clean: true,
   },
+  devtool: "eval-source-map",
   plugins: [
     new HTMLWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
     }),
     new webpack.ProgressPlugin(),
+    new Dotenv({
+      systemvars: true
+    })
   ],
   module: {
     rules: [
@@ -23,6 +27,14 @@ const config: webpack.Configuration = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
     ],
   },
   resolve: {
